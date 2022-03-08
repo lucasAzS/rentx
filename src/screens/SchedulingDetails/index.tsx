3d900647fda3822b2dc8bf346f41ff20678023f9
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-03-08 16:28:48 -0300

    Schedule Details Interface

---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+
+import { Feather } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  CarImages,
+  Container,
+  Header,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  Footer,
+  RentalPeriod,
+  CaledarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from './styles';
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            'https://uno.fiat.com.br/content/dam/fiat/products/195/a4z/2/2021/page/profile.png',
+          ]}
+        />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Fiat</Brand>
+            <Name>Uno</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 100,00</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name='380Km/h' icon={SpeedSvg} />
+          <Accessory name='3.2s' icon={AccelerationSvg} />
+          <Accessory name='800 HP' icon={ForceSvg} />
+          <Accessory name='Gasolina' icon={GasolineSvg} />
+          <Accessory name='Auto' icon={ExchangeSvg} />
+          <Accessory name='2 Pessoas' icon={PeopleSvg} />
+        </Accessories>
+
+        <RentalPeriod>
+          <CaledarIcon>
+            <Feather
+              name='calendar'
+              size={RFValue(28)}
+              color={theme.colors.shape}
+            />
+          </CaledarIcon>
+
+          <DateInfo>
+            <DateTitle>De</DateTitle>
+            <DateValue>08/03/2022</DateValue>
+          </DateInfo>
+
+          <Feather
+            name='chevron-right'
+            size={RFValue(16)}
+            color={theme.colors.text}
+          />
+
+          <DateInfo>
+            <DateTitle>De</DateTitle>
+            <DateValue>08/03/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title='Confirmar' />
+      </Footer>
+    </Container>
+  );
+}
