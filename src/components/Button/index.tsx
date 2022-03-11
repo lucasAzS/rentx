c8f12a59841e9090694e4b9de21933e1508aa61e
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-03-11 18:09:17 -0300

    Button Usability

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,11 +6,17 @@ interface Props {
   title: string;
   color?: string;
   onPress: () => void;
+  enabled?: boolean;
 }
 
-export function Button({ title, color, onPress }: Props) {
+export function Button({ title, color, onPress, enabled = true }: Props) {
   return (
-    <Container color={color} onPress={onPress}>
+    <Container
+      color={color}
+      onPress={onPress}
+      enabled={enabled}
+      style={{ opacity: enabled ? 1 : 0.5 }}
+    >
       <Title>{title}</Title>
     </Container>
   );
--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -136,7 +136,11 @@ export function Scheduling() {
       </Content>
 
       <Footer>
-        <Button title='Confirmar' onPress={handleConfirmRental} />
+        <Button
+          title='Confirmar'
+          onPress={handleConfirmRental}
+          enabled={!!rentalPeriod.startFormatted}
+        />
       </Footer>
     </Container>
   );
