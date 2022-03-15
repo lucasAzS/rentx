94dc3b060b5a87b67ad44c634462678c6673e694
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-03-15 19:33:43 -0300

    Primeira Animação

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
 
 import { Container } from './styles';
 
 export function Splash() {
-  return <Container></Container>;
+  const animation = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: animation.value }],
+    };
+  });
+
+  function handleAnimationPosition() {
+    animation.value = Math.random() * 300 - 100;
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styles.box, animatedStyle]} />
+      <Button title='Mover' onPress={handleAnimationPosition} />
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    margin: 16,
+  },
+});
