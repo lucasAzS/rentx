7025a826657475715723ac63839a6c676fc7ee2b
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-03-18 16:18:41 -0300

    Gestos do Usuário

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -42,18 +42,3 @@ export const CarList = styled(
   },
   showsVerticalScrollIndicator: false,
 })``;
-
-export const MyCarsButton = styled(RectButton)`
-  width: ${RFValue(60)}px;
-  height: ${RFValue(60)}px;
-
-  border-radius: ${RFValue(30)}px;
-
-  background-color: ${({ theme }) => theme.colors.main};
-  justify-content: center;
-  align-items: center;
-
-  position: absolute;
-  bottom: 13px;
-  right: 22px;
-`;
