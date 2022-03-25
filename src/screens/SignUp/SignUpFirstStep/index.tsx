beb1c3e8de3c94dc1c8f99b4db2550f469d9e3d8
Lucas de Azambuja dos Santos <lucasazsantos@gmail.com>
2022-03-25 16:12:57 -0300

    Primeiro Step

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import {
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import { BackButton } from '../../../components/BackButton';
+import { Input } from '../../../components/Input';
+import { Button } from '../../../components/Button';
+
+import { Bullet } from '../../../components/Bullet';
+
 import {
   Container,
   Header,
@@ -11,7 +21,6 @@ import {
   Form,
   FormTitle,
 } from './styles';
-import { Bullet } from '../../../components/Bullet';
 
 export function SignUpFirstStep() {
   const navigation = useNavigation<any>();
@@ -21,24 +30,30 @@ export function SignUpFirstStep() {
   }
 
   return (
-    <Container>
-      <Header>
-        <BackButton onPress={handleBack} />
-        <Steps>
-          <Bullet active />
-          <Bullet />
-        </Steps>
-      </Header>
-
-      <Title>Crie sua{'\n'}conta</Title>
-      <Subtitle>
-        Faça seu cadastro de{'\n'}
-        forma rápida e fácil
-      </Subtitle>
-
-      <Form>
-        <FormTitle>1. Dados</FormTitle>
-      </Form>
-    </Container>
+    <KeyboardAvoidingView behavior='position' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+              <Bullet active />
+              <Bullet />
+            </Steps>
+          </Header>
+          <Title>Crie sua{'\n'}conta</Title>
+          <Subtitle>
+            Faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </Subtitle>
+          <Form>
+            <FormTitle>1. Dados</FormTitle>
+            <Input iconName='user' placeholder='Nome' />
+            <Input iconName='mail' placeholder='Email' />
+            <Input iconName='credit-card' placeholder='CNH' />
+          </Form>
+          <Button title='Proximo' />
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
